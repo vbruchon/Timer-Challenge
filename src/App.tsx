ca4f75892e64e8f2025ee32c5f4151bed9c43143
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2024-06-26 04:09:42 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="mx-auto flex min-h-screen max-w-3xl flex-col gap-8 p-4">
+            <h1 className="text-center text-2xl">Timer Challenge</h1>
+        </div>
+    )
+}
+
+export default App
